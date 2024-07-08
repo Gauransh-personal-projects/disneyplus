7fecdd5467785caf937bc375f06f1ce290a23bcf
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2024-07-08 09:47:33 +0000

    feat: Add search functionality to search page

---
+++ app/search/[term]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+type Props = {  
+    params: {
+        term: string
+    }
+}
+
+function Searchpage({params: {term}}: Props ) {
+
+    if (!term) notFound();
+
+    const termToUSe = decodeURI(term);
+
+    // API call to get the searched movies
+
+    // API call to get the popular movies
+
+    return (
+        <div>
+        Welcome to the search page: {termToUSe}
+        </div>
+    )
+}
+
+export default Searchpage;
