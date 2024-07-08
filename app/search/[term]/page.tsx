83041be082a96010c0a32428bd8bd5483203ca28
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2024-07-08 20:26:15 +0000

    chore: Add Azure Functions configuration files and dependencies

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MoviesCarousel from '@/components/MoviesCarousel';
+import { getPopularMovies, getSearchedMovies } from '@/lib/getMovies';
 import { notFound } from 'next/navigation'
 
 type Props = {  
@@ -6,19 +8,28 @@ type Props = {
     }
 }
 
-function Searchpage({params: {term}}: Props ) {
+async function Searchpage({params: {term}}: Props ) {
 
     if (!term) notFound();
 
     const termToUSe = decodeURI(term);
 
     // API call to get the searched movies
+    const movies = await getSearchedMovies(termToUSe);
 
     // API call to get the popular movies
+    const popularMovies = await getPopularMovies();
 
     return (
-        <div>
-        Welcome to the search page: {termToUSe}
+        <div className='max-w-7xl mx-auto'>
+            <div className='flex flex-col space-y-4 mt-32 xl:mt-42'>
+                <h1 className='text-6xl font-bold px-10'>Results for {termToUSe}</h1>
+
+                {/* AI Suggestion */}
+
+                <MoviesCarousel movies={movies} title="Movies" isVertical />
+                <MoviesCarousel movies={popularMovies} title="You may also like" />
+            </div>
         </div>
     )
 }
